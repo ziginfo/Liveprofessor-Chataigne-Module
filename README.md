0e457b87ed18f754c291a13243222510ff029073
ziginfo <34913387+ziginfo@users.noreply.github.com>
2024-03-31 15:45:33 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,10 @@
+## Audioström' Liveprofessor control with chataigne by OSC
+Default ports are set to  8000 (OUT) and 8001 (IN). But can be changed in the preferences of Live Professor!
+
+I just started to write this module-script. So it is still totally "Work-in-Progress"...   
+Pleas contact me if you have demands or requests and any help is always welcome !!
+
+Have Fun
+
+To learn more about Chataigne, please visit : http://benjamin.kuperberg.fr/chataigne/    
+And Ben's Youtube channel where you can find tutorials : https://youtu.be/RSBU9MwJNLY
---
+++ liveprofessor.js
@@ -0,0 +1,189 @@
+// ========================== VARS ===========================
+
+var snapcount = count = local.values.numberOfSnapshots.get() ;
+var chaincount = count = local.values.numberOfChains.get() ;
+
+//====================================================================
+//			INITIAL FUNCTIONS 
+//====================================================================
+
+function init() {
+
+// =====================================================================
+// 			CREATE CONTAINERS
+// =====================================================================
+
+Snap = local.values.addStringParameter("Active Snapshot", "Active Snapshot","Active Snapshot");
+activeCue = local.values.addStringParameter("Active Cue", "Active Cue","Active Cue");
+nextCue = local.values.addStringParameter("Next Cue", "Next Cue","Next Cue");
+
+
+//Snaphot Labels Container >>>>>>>>>>>>>>>>>>>>>>		
+		snap=local.values.addContainer("Snapshot Labels");
+		snap.setCollapsed(true);
+		snap.addTrigger("Sync Labels", "Get Labels from Liveprofessor" , false);
+		snap.addTrigger("Reset Labels", "Reset All Labels" , false);	
+		for (var n = 1; n <= snapcount; n++) {
+			snap.addStringParameter("Snapshot "+n, "", ""); }
+			
+// Chain Faders Container>>>>>>>>>>>>>>>>>>>>>
+		
+		faders = local.values.addContainer("Chain Gains");
+		faders.setCollapsed(true);	
+		faders.addTrigger("Reset Values", "Reset All Values" , false);
+		faders.addTrigger("Sync Gains", "Get Fader Values from Liveprofessor" , false);	
+		for (var n = 1; n <= chaincount; n++) {
+			var fade = faders.addFloatParameter("Chain "+n+" In", "", 0, 0, 1);
+			fade.setAttribute("readonly" ,true);
+			var fade = faders.addFloatParameter("Chain "+n+" Out", "", 0, 0, 1);
+			fade.setAttribute("readonly" ,true);  }
+			
+			
+}
+
+// =====================================================================
+// 			PARAM CHANGES
+// =====================================================================
+
+function moduleParameterChanged(param) {
+  script.log(param.name + " parameter changed, new value: " + param.get());
+}
+
+// =====================================================================
+// 			VALUE CHANGES
+// =====================================================================
+
+function moduleValueChanged(value) {
+  
+  	if (value.name == "syncGains"){ 
+  	local.send("/GlobalSnapshots/Refresh"); }
+  	
+  	if (value.name == "syncLabels"){ 
+  	local.send("/GlobalSnapshots/Refresh"); }
+  	
+  	if (value.name == "resetLabels"){ 
+  	for (var n = 0; n < snapcount; n++) {
+	var no = n+1 ;
+	local.values.snapshotLabels.getChild('Snapshot'+no).set(""); 
+	} }
+    
+  
+}
+
+//============================================================
+//			OSC EVENTS
+//============================================================
+
+function oscEvent(address, args) {
+
+// >>> active and next Cue
+	if (address == "/GlobalSnapshots/Recalled")
+ 	{local.values.activeSnapshot.set(args[0]);}	
+ 	if (address == "/CueLists/ActiveCue")
+ 	{local.values.activeCue.set(args[0]);}
+	if (address == "/CueLists/NextCue")
+ 	{local.values.nextCue.set(args[0]);}
+
+// >>> insert Snapshot Labels	
+	for (var n = 0; n < snapcount; n++) {
+	var no = n+1 ;
+	var addr = "/GlobalSnapshots/Name" ;
+	var child = "Snapshot"+no ;
+	if (address == addr) {
+	var snap = args[1] ;
+	if (snap == n) 
+	{local.values.snapshotLabels.getChild('Snapshot'+no).set(args[0]);} 
+	} }
+
+}
+// =====================================================================
+// 			GENEREIC FUNCTIONS
+// =====================================================================
+
+/// ===========Snapshots  ==================
+
+function set_setlist(val) {
+	val = val-1 ;
+	local.send("/GigPerformer/SelectSetList", val);
+}
+
+function set_snap(val) {
+	val = val-1 ;
+	local.send("/GlobalSnapshots/Recall", val);
+}
+
+function prev_snap() {
+	local.send("/Command/GlobalSnapshots/RecallPreviousGlobalSnapshot");
+}
+
+function next_snap() {
+	local.send("/Command/GlobalSnapshots/RecallNextGlobalSnapshot");
+}
+
+function update_snap() {
+	local.send("/Command/GlobalSnapshots/UpdateActiveGlobalSnapshot");
+}
+
+/// ===========Cues  ==================
+
+function cue_down() {
+	local.send("/Command/CueLists/StepDown");
+}
+
+function cue_up() {
+	local.send("/Command/CueLists/StepUp");
+}
+
+function recall_cue(val) {
+	val = val-1 ;
+	local.send("/Cue/Recall", val);
+}
+
+function prev_cue() {
+	local.send("/Command/CueLists/FirePreviousCue");
+}
+
+function next_cue() {
+	local.send("/Command/CueLists/FireNextCue");
+}
+
+function clear_songpart() {
+	local.send("/SongPart/ClearSnapshot");
+}
+
+/// =========== Chain Control =============
+
+function recall_rack(val) {
+	val = val-1 ;
+	local.send("/GigPerformer/SwitchToRack", val);
+}
+
+function recall_rackspace(val) {
+	val = val-1 ;
+	local.send("/GigPerformer/SwitchToRackSpace", val);
+}
+
+function prev_space() {
+	local.send("/GigPerformer/PrevRackSpace");
+}
+
+function next_space() {
+	local.send("/GigPerformer/NextRackSpace");
+}
+
+function prev_variation() {
+	local.send("/RackSpace/PrevVariation");
+}
+
+function next_variation() {
+	local.send("/RackSpace/NextVariation");
+}
+
+function move_up() {
+	local.send("/GigPerformer/MoveUp");
+}
+
+
+function move_down() {
+	local.send("/GigPerformer/MoveDown");
+}
